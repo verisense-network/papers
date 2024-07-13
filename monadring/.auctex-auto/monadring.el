57e2df39432d68f78735071636081f51e41f41d7
0xed1s0n <0xed1s0n@proton.me>
2024-07-13 20:29:27 +0800

    add author and org

--- monadring/.auctex-auto/monadring.el
+++ monadring/.auctex-auto/monadring.el
@@ -1,9 +1,34 @@
 (TeX-add-style-hook
  "monadring"
  (lambda ()
-   (LaTeX-add-bibitems
-    "grandpa"
-    "10.1007/978-3-642-32009-5_50"
-    "cryptoeprint:2012/144"))
- '(or :bibtex :latex))
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "inputenc"
+    "fontenc"
+    "graphicx"
+    "longtable"
+    "wrapfig"
+    "rotating"
+    "ulem"
+    "amsmath"
+    "amssymb"
+    "capt-of"
+    "hyperref"
+    "authblk"
+    "draftwatermark")
+   (LaTeX-add-bibliographies))
+ :latex)
 
